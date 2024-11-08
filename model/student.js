4df5e85b344ba62646d9f73f7823f803c086d6c2
hajimonsur <taiwomonsuru29@gmail.com>
2024-11-08 19:20:50 +0100

    created a server and file structure

--- model/student.js
+++ model/student.js
@@ -1,1 +1,9 @@
-// model schema will be here
+
+const mongoose = require("mongoose");
+const studentSchema = new mongoose.Schema({
+
+    // model schema will be here
+});
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
--- routes/studentroute.js
+++ routes/studentroute.js
@@ -1,1 +1,10 @@
 // student route will here
+const express = require("express");
+const router = express.Router();
+const Student = require("../model/Student");
+
+
+
+
+
+module.exports = router;
