91d9769ce27c3c07a89eed9d516ba669c0971ad7
hajimonsur <taiwomonsuru29@gmail.com>
2024-11-09 17:22:35 +0100

    all-students route done

--- routes/studentroute.js
+++ routes/studentroute.js
@@ -3,7 +3,19 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../model/Student");
 
-
+// get all students
+router.get("/all-students", async (req, res) => {
+  
+    try {
+        const students = await Student.find();
+        res.status(200).json({
+            success: true,
+            data: students
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 
 
